190f74fe17699bca9cfe178e600923595e7f6ebf
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-06 17:06:22 -0300

    Update Ex06.java

--- src/Ex06.java
+++ src/Ex06.java
@@ -1,4 +1,14 @@
 package PACKAGE_NAME;
 
 public class Ex06 {
+   static void main() {
+
+        int cont=0;
+        for (int i = 1; i <= 5; i++) {
+            cont += i;
+
+        }
+        IO.println(cont);
+
+    
 }
