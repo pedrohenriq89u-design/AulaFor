5aa852022c28f722136ffe95e7fe7bb483e88032
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-06 17:05:38 -0300

    Update main method to print odd numbers
    Refactored loop to print odd numbers from 1 to 9.

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,4 +1,15 @@
 package PACKAGE_NAME;
 
 public class Ex04 {
+  static void main(){
+//        for (int i = 1; i <= 9; i++){
+//            if (i % 2 == 1){
+//            IO.println(i);}
+//        }
+
+        for (int i = 1; i <= 9; i+=2){
+
+                IO.println(i);
+        }
+    
 }
