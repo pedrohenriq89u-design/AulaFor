4e3c673e254ec1525e8671729a06f5a97ef4a641
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-06 17:06:43 -0300

    Add multiplication table feature to Ex07

--- src/Ex07.java
+++ src/Ex07.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
 
 public class Ex07 {
+  static void main(){
+        Scanner sc = new Scanner(System.in);
+        IO.println("Escolha a tabuada:");
+        int cont = sc.nextInt();
+       for ( int i = 1; i <=10; i++){
+
+        IO.println(  cont * i);
+        }
+    
 }
