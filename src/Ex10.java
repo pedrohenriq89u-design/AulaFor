30b3fb40d11c4f726a55ac4479b0d41aaa420a05
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-06 17:07:47 -0300

    Implement main method to calculate average grades

--- src/Ex10.java
+++ src/Ex10.java
@@ -1,4 +1,20 @@
 package PACKAGE_NAME;
 
 public class Ex10 {
+  static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        double cont = 0;
+        IO.println("Digite suas notas:");
+        for (double i = 1; i <=5;i++) {
+        double notas = sc.nextInt();
+            cont+=notas;
+
+            //IO.println("Sua média é: "+cont/5);
+
+        }
+
+        IO.println("Sua média é: "+cont/5);
+    }
+
 }
