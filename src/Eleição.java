c45298ab91340b4d091ed57fb236be4ee4274109
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-06 17:08:25 -0300

    Implement voting system in Eleição class

--- src/Eleição.java
+++ src/Eleição.java
@@ -1,4 +1,31 @@
 package PACKAGE_NAME;
 
 public class Eleição {
+   static void main() {
+        Scanner sc = new Scanner(System.in);
+        IO.println("Quantas Pessoas iram votar?");
+        int oi = sc.nextInt();
+        IO.println(" Candidatos:");
+        IO.println("1- João");
+        IO.println("2- Maria");
+        for (int i = 1; i <= oi; i++) {
+            IO.println("Digite");
+            int ai = sc.nextInt();
+
+            {
+
+                if (ai == 1) {
+                   IO.println("João teve " + ai + " voto");
+                }else {
+                    IO.println("Maria teve" + ai + " voto");
+                }
+            }
+
+                //for (int i = 1; i <= oi; i++) ;
+                {
+
+                }
+            }
+
+        
 }
