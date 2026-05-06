5a5a331195c93f2637b92731f823f3081757f292
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-05-06 17:07:05 -0300

    Update Ex08.java

--- src/Ex08.java
+++ src/Ex08.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
 
 public class Ex08 {
+   static void main(){
+        int cont = 0;
+        for (int i = 1; i <=100; i++) {
+            cont += i;
+
+
+        }
+        IO.println(cont);
+    
 }
